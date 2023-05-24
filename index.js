154783598e2865de5a01fc34a99715c927823529
JsGv1405 <jsgv1405@gmail.com>
2023-05-23 23:08:51 -0500

    i feel better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
-import { uploadFile, getFiles, getFile, downloadFile, getFileURL } from './s3.js'
+import { uploadFile } from './s3.js'
 
 const app = express()
 
@@ -8,23 +8,6 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './uploads'
 }))
-
-app.get('/files', async (req, res) => {
-    const result = await getFiles()
-    res.json(result.Contents)
-})
-
-app.get('/files/:fileName', async (req, res) => {
-    const result = await getFileURL(req.params.fileName)
-    res.json({
-        url: result
-    })
-})
-
-app.get('/downloadfile/:fileName', async (req, res) => {
-    await downloadFile(req.params.fileName)
-    res.json({message: "archivo descargado"})
-})
 
 
 app.post('/files', async (req, res) => {
@@ -34,5 +17,5 @@ app.post('/files', async (req, res) => {
 
 app.use(express.static('images'))
 
-app.listen(3000)
-console.log(`Server on port ${3000}`)
+app.listen(3001)
+console.log(`Server on port ${3001}`)
--- s3.js
+++ s3.js
@@ -1,13 +1,14 @@
 import { S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand } from '@aws-sdk/client-s3'
-import { AWS_BUCKET_REGION, AWS_PUBLIC_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME } from './config.js'
+
 import fs from 'fs'
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
 
+const AWS_BUCKET_NAME='tesisloja';
 const client = new S3Client({
-    region: AWS_BUCKET_REGION,
+    region: 'us-east-1',
     credentials: {
-        accessKeyId: AWS_PUBLIC_KEY,
-        secretAccessKey: AWS_SECRET_KEY
+        accessKeyId: 'AKIAU2R2UBQ7YC3KRFUP',
+        secretAccessKey: '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'
     }
 })
 
@@ -22,35 +23,3 @@ export async function uploadFile(file) {
     return await client.send(command)
 }
 
-export async function getFiles() {
-    const command = new ListObjectsCommand({
-        Bucket: AWS_BUCKET_NAME
-    })
-    return await client.send(command)
-}
-
-export async function getFile(filename) {
-    const command = new GetObjectCommand({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filename
-    })
-    return await client.send(command)
-}
-
-export async function downloadFile(filename) {
-    const command = new GetObjectCommand({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filename
-    })
-    const result = await client.send(command)
-    console.log(result)
-    result.Body.pipe(fs.createWriteStream(`./images/${filename}`))
-}
-
-export async function getFileURL(filename) {
-    const command = new GetObjectCommand({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filename
-    })
-    return await getSignedUrl(client, command, { expiresIn: 3600 })
-}
